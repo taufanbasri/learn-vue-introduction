66eae98bc0a2fbed074c9fdba50bfc8248fe1d35
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-02-25 22:47:41 +0700

    Use VueJS

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const buttonElement = document.querySelector('button');
-const inputElement = document.querySelector('input');
-const listElement = document.querySelector('ul');
+// Use Just JavaScript
+// const buttonElement = document.querySelector('button');
+// const inputElement = document.querySelector('input');
+// const listElement = document.querySelector('ul');
 
-buttonElement.addEventListener('click', addGoal);
+// buttonElement.addEventListener('click', addGoal);
 
-function addGoal() {
-    const enteredValue = inputElement.value;
-    const listItemElement = document.createElement('li');
+// function addGoal() {
+//     const enteredValue = inputElement.value;
+//     const listItemElement = document.createElement('li');
 
-    listItemElement.textContent = enteredValue;
+//     listItemElement.textContent = enteredValue;
 
-    listElement.append(listItemElement);
+//     listElement.append(listItemElement);
 
-    inputElement.value = '';
-}
+//     inputElement.value = '';
+// }
+
+// Use VueJS
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = ''
+        }
+    },
+}).mount('#app');
